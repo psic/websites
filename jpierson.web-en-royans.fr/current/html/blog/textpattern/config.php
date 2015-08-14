4e320620ee3507c166dac01e1b5f93c49d2dfc9f
psic <psic@free.fr>
2015-08-14 12:19:23 +0200

    first commit

---
+++ jpierson.web-en-royans.fr/current/html/blog/textpattern/config.php
@@ -0,0 +1,9 @@
+<?
+$txpcfg['db'] = 'blog';
+$txpcfg['user'] = 'blog';
+$txpcfg['pass'] = 'Bl0gg0L';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = 'prem_';
+$txpcfg['txpath'] = '/var/www/grenoble-informatique/html/blog/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
